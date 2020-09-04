3a7ab688fc92e1254c34220db2a695e44a43707a
Jared Schau <deraj320@hotmail.com>
2020-09-04 07:16:34 -0600

    Updates

---
+++ precache-manifest.795f1df6310d9cb05a5a1a9e8f7d05fa.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "580d8e4f18e980f3e9abffa0ffb88577",
+    "url": "/portfolio-react/index.html"
+  },
+  {
+    "revision": "870b996df0b457bf293c",
+    "url": "/portfolio-react/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "fa3d19f24af6bdb37ca2",
+    "url": "/portfolio-react/static/css/main.d73a1f26.chunk.css"
+  },
+  {
+    "revision": "870b996df0b457bf293c",
+    "url": "/portfolio-react/static/js/2.987377e7.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/portfolio-react/static/js/2.987377e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa3d19f24af6bdb37ca2",
+    "url": "/portfolio-react/static/js/main.c80f5179.chunk.js"
+  },
+  {
+    "revision": "feb6fec8f95565f3aa5d",
+    "url": "/portfolio-react/static/js/runtime-main.c94dc782.js"
+  },
+  {
+    "revision": "bb9ee0694f3b913b499c1dd8fd571b10",
+    "url": "/portfolio-react/static/media/day.bb9ee069.png"
+  },
+  {
+    "revision": "ea246099afe96c6866538bd014cdddcd",
+    "url": "/portfolio-react/static/media/moodmusic.ea246099.png"
+  },
+  {
+    "revision": "d8fc5b83bcf249e609f8b33dd762ab9f",
+    "url": "/portfolio-react/static/media/mountains.d8fc5b83.jpg"
+  },
+  {
+    "revision": "1397ec0664be527200a97f30a60074f5",
+    "url": "/portfolio-react/static/media/password.1397ec06.png"
+  },
+  {
+    "revision": "672f066451b7345913a0fba8c89a3414",
+    "url": "/portfolio-react/static/media/project2.672f0664.png"
+  }
+]);
